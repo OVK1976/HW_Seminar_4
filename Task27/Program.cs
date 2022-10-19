557586df3975818b978794296371c9409339d2a2
OVK1976 <olegkalinin@yandex.ru>
2022-10-19 09:02:43 +0400

    Готовая версия

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Пример:
+452-> 11
+82-> 10
+9012-> 12 */
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+SumNum(num);
+#region Function
+int SumNum(int Number)
+{
+    int sum = 0;
+    
+        while (Number > 0)
+        {
+            sum = sum + Number % 10;
+            Number = Number / 10;
+        }
+        Console.WriteLine($"Сумма чисел равна {sum}");
+
+    return sum;
+}
+#endregion
